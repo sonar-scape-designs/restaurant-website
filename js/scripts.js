e7a8df7f50152f340217af5433355fbcc2e66d7c
sonar-scape-designs <57213648+sonar-scape-designs@users.noreply.github.com>
2020-02-11 11:26:55 +0100

    categories linked to products

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,55 @@
-fetch("https://kea-alt-del.dk/t5/api/productlist")
-.then(function (display) {
-    return display.json()
+fetch("https://kea-alt-del.dk/t5/api/categories")
+    .then(function (display) {
+        return display.json()
     })
-.then(function (database){
-    uploadData(database)
-})
+    .then(displayCategories)
 
-function uploadData(onlineDatabase) {
-    onlineDatabase.forEach(showData)
-    console.log(onlineDatabase)
+function displayCategories(data) {
+//    console.log(data)
+    data.forEach(function (eachItem) {
+
+        const section = document.createElement("section");
+        section.id = eachItem;
+        const h2 = document.createElement("h2");
+        h2.textContent = eachItem;
+        section.appendChild(h2);
+        document.querySelector("main").appendChild(section);
+    })
+showProducts();
 
 }
 
 
-function showData(jsonDatabase){
-//    console.log("matter")
-    const template = document.querySelector("#starter-template").content;
+function showProducts() {
+    fetch("https://kea-alt-del.dk/t5/api/productlist")
+        .then(function (display) {
+            return display.json()
+        })
+        .then(function (database) {
+            uploadData(database)
+        })
+
+    function uploadData(onlineDatabase) {
+        onlineDatabase.forEach(showData)
+//        console.log(onlineDatabase)
+
+    }
+}
+
+function showData(jsonDatabase) {
+//        console.log(jsonDatabase, "this is single data")
+    const template = document.querySelector("#saleItems").content;
     const clone = template.cloneNode(true);
 
     clone.querySelector("h2").textContent = jsonDatabase.name;
     clone.querySelector("h3 span").textContent = jsonDatabase.price;
     clone.querySelector("p").textContent = jsonDatabase.shortdescription;
     clone.querySelector(".discount").textContent = jsonDatabase.discount;
-//    clone.querySelector("img").src=heroDatabase.selfie;
+//        clone.querySelector("img").src=heroDatabase.selfie;
 
-    //const parent = document.querySelector("main");
-    document.querySelector("main").appendChild(clone)
+//    const parent = document.querySelector("main");
+//    document.querySelector("main").appendChild(clone)
 
+    console.log(`#${jsonDatabase.category}`)
+    document.querySelector(`#${jsonDatabase.category}`).appendChild(clone);
 }
